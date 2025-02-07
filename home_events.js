1cbec8e51ca8d95eda933eddd73e950f4516507c
Ilinca Popovici <ilincapopovici2000@yahoo.com>
2025-02-07 16:04:08 +0200

    add improvements

--- home_events.js
+++ home_events.js
@@ -1,28 +1,39 @@
 function displayTextService(button) {
-    // Find the parent `.service-item` of the clicked button
-    const serviceItem = button.closest(".service-item");
+  // Find the parent `.service-item` of the clicked button
+  const serviceItem = button.closest(".service-item");
   
-    // Find the `.description` element within the same `.service-item`
-    const description = serviceItem.querySelector(".description-service");
+  // Find the `.description` element within the same `.service-item`
+  const description = serviceItem.querySelector(".description-service");
   
-    // Show the description and hide the button
-    if (description) description.style.display = "block";
-    button.style.display = "none";
-  }
+  // Show the description and hide the button
+  if (description) description.style.display = "block";
+  button.style.display = "none";
+}
 
-  function displayTextArticle(button) {
-    // Find the parent `.article-item` of the clicked button
-    const articleItem = button.closest(".article-item");
+function displayTextArticle(button) {
+  // Find the parent `.article-item` of the clicked button
+  const articleItem = button.closest(".article-item");
   
-    // Find the `.description` element within the same `.article-item`
-    const description = articleItem.querySelector(".description-article");
+  // Find the `.description` element within the same `.article-item`
+  const description = articleItem.querySelector(".description-article");
   
-    // Show the description and hide the button
-    if (description) {
-      description.style.display = "block";
-      articleItem.style.height = `${articleItem.scrollHeight + 590}px`;
-    }
-    button.style.display = "none";
+  // Show the description and hide the button
+  if (description) {
+    description.style.display = "block";
+    articleItem.style.height = `${articleItem.scrollHeight + 600}px`;
   }
+  button.style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuCheckbox = document.getElementById("menu-bar");
+  const menuLinks = document.querySelectorAll(".nav-link");
+
+  menuLinks.forEach(link => {
+      link.addEventListener("click", () => {
+          menuCheckbox.checked = false; // Uncheck the menu checkbox
+      });
+  });
+});
 
   
